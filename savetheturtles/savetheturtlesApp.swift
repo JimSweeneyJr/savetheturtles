3652a6d4eae04636169b140372dff768ea136cd6
JimSweeneyJr <jimsweeney566@gmail.com>
2023-11-08 21:03:41 -0500

    Initial Commit

---
+++ savetheturtles/savetheturtlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  savetheturtlesApp.swift
+//  savetheturtles
+//
+//  Created by James Sweeney on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct savetheturtlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
