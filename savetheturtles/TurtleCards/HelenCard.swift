23f19a0e46dba2966db8989f048c4ad9c8b830ab
JimSweeneyJr <jimsweeney566@gmail.com>
2023-11-10 17:10:02 -0500

    healthBar added

--- savetheturtles/TurtleCards/HelenCard.swift
+++ savetheturtles/TurtleCards/HelenCard.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct HelenCard: View {
+    
+    @State var progressValue: Float = 0.0
+    
     var body: some View {
-        Text("Hi, I am helen")
+        VStack {
+            LifeBarView(progress: self.$progressValue)
+                .frame(width: 160.0, height: 160.0)
+                .padding(20.0)
+                .onAppear() {
+                    self.progressValue = 0.50
+                }
+            
+            Button("Increment") {
+                if (progressValue) < 1.0 {
+                    self.progressValue += 0.10
+                } else {
+                    progressValue -= 1.0
+                }
+            }
+        }
     }
 }
 
